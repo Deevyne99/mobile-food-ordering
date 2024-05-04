e20fb6eee7fbddf3b002731dce3fdd52fc80872e
Deevyne99 <kaludivine545@gmail.com>
2024-05-04 11:18:28 +0100

    Style: Updated Products screen Ui

--- food-ordering/src/app/(tabs)/index.tsx
+++ food-ordering/src/app/(tabs)/index.tsx
@@ -1,6 +1,9 @@
-import products from '@/assets/data/products'
-import Colors from '@/src/constants/Colors'
-import { StyleSheet, Text, View, Image } from 'react-native'
+ProductListComponent
+products
+import ProductListComponent from '@//components/ProductListComponent'
+// import Colors from '@/src/constants/Colors'
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native'
+import products from '../../../assets/data/products'
 
 // import EditScreenInfo from '@/src/components/EditScreenInfo'
 // import { } from '@/src/components/Themed'
@@ -8,32 +11,14 @@ import { StyleSheet, Text, View, Image } from 'react-native'
 export default function TabOneScreen() {
   const product = products[0]
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: product.image }} style={styles.image} />
-      <Text style={styles.title}>{product.name}</Text>
-      <Text style={styles.price}>${product.price}</Text>
+    <View>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListComponent product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10, padding: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+      />
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  },
-
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 10,
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold',
-  },
-})
---
+++ food-ordering/src/components/ProductListComponent.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { Product } from '../types'
+import Colors from '../constants/Colors'
+// import EditScreenInfo from '@/src/components/EditScreenInfo'
+// import { } from '@/src/components/Themed'
+type ProductListProps = {
+  product: Product
+}
+
+export default function ProductListComponent({ product }: ProductListProps) {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={{
+          uri:
+            product.image ||
+            'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png',
+        }}
+        style={styles.image}
+        resizeMode='contain'
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+    flex: 1,
+    maxWidth: '50%',
+    // margin: 10,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1,
+  },
+
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold',
+  },
+})
